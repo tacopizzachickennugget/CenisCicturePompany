eb953022d052f7ebd159f587890b94be27afd3c9
JJohnson200 <phedre357@gmail.com>
2018-12-08 20:13:01 -0800

    YarnBuyingStringTest - woo!

---
+++ Test.py
@@ -0,0 +1,7 @@
+for i in range(1,100):
+    print ("I bought some yarn today, Andrew.")
+    print ("How much yarn, Jessica?")
+    print (i, " skeins")
+print("I have a lot of yarn. I should open a store.")
+
+
